f2272d88937a881b05bbac20d722c3c859418b4c
Thindal <thindaltv@gmail.com>
2024-06-24 20:39:59 +0200

    Updated code with intellisense comments. Also marked methods in UntilExtensions that need unit tests.

--- src/Thindal.Strings/Extensions/SubstringOrShorterExtensions.cs
+++ src/Thindal.Strings/Extensions/SubstringOrShorterExtensions.cs
@@ -2,6 +2,11 @@
 
 public static class SubstringOrShorterExtensions
 {
+    /// <summary>
+    /// Retrieves a substring from this instance. If the string is shorter than the length, the whole string is returned.
+    /// </summary>
+    /// <param name="length">The requested length of the string.</param>
+    /// <returns>A string the length of <paramref name="length"/> or shorter.</returns>
     public static string SubstringOrShorter(this string str, int length)
     {
         if (str.Length <= length)
@@ -10,6 +15,12 @@ public static class SubstringOrShorterExtensions
         return str.Substring(0, length);
     }
 
+    /// <summary>
+    /// Retrieves a substring from this instance starting at <paramref name="startIndex"/>. If the string is shorter than the length, the whole string is returned.
+    /// </summary>
+    /// <param name="startIndex">The index to start the substring from</param>
+    /// <param name="length">The requested length of the string</param>
+    /// <returns>A string the length of <paramref name="length"/> or shorter, starting at <paramref name="startIndex"/></returns>
     public static string SubstringOrShorter(this string str, int startIndex, int length)
     {
         if (str.Length <= startIndex)
--- src/Thindal.Strings/Extensions/UntilExtensions.cs
+++ src/Thindal.Strings/Extensions/UntilExtensions.cs
@@ -1,6 +1,15 @@
 ﻿namespace System;
+/// <summary>
+/// Provides extension methods for the <see cref="string"/> class to extract substrings until a specified delimiter.
+/// </summary>
 public static class UntilExtensions
 {
+    /// <summary>
+    /// Returns a substring from the start of the input string until the first occurrence of the specified delimiter.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(this string str, string until)
     {
         if (string.IsNullOrEmpty(until))
@@ -13,8 +22,16 @@ public static class UntilExtensions
         return str.Substring(0, index);
     }
 
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified delimiter.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(string str, int startIndex, string until)
     {
+        // TODO: Needs tests
         if (string.IsNullOrEmpty(until))
             return str;
 
@@ -22,12 +39,18 @@ public static class UntilExtensions
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
 
-    // indexof char
+    /// <summary>
+    /// Returns a substring from the start of the input string until the first occurrence of the specified character.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter character.</param>
+    /// <returns>The substring until the first occurrence of the character, or the original string if the character is not found.</returns>
     public static string Until(this string str, char until)
     {
+        // TODO: Needs tests
         var index = str.IndexOf(until);
         if (index == -1)
             return str;
@@ -35,29 +58,51 @@ public static class UntilExtensions
         return str.Substring(0, index);
     }
 
-    // indexof char, startindex
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified character.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter character.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <returns>The substring until the first occurrence of the character, or the original string if the character is not found.</returns>
     public static string Until(this string str, char until, int startIndex)
     {
+        // TODO: Needs tests
         var index = str.IndexOf(until, startIndex);
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
 
-    // indexof char, startindex, count
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified character within the specified count.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter character.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <param name="count">The maximum number of characters to consider.</param>
+    /// <returns>The substring until the first occurrence of the character, or the original string if the character is not found.</returns>
     public static string Until(this string str, char until, int startIndex, int count)
     {
+        // TODO: Needs tests
         var index = str.IndexOf(until, startIndex, count);
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
 
-    // indexof char, StringComparison
+    /// <summary>
+    /// Returns a substring from the start of the input string until the first occurrence of the specified character using the specified comparison type.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter character.</param>
+    /// <param name="comparisonType">The comparison type to use.</param>
+    /// <returns>The substring until the first occurrence of the character, or the original string if the character is not found.</returns>
     public static string Until(this string str, char until, StringComparison comparisonType)
     {
+        // TODO: Needs tests
         var index = str.IndexOf(until, comparisonType);
         if (index == -1)
             return str;
@@ -65,7 +110,13 @@ public static class UntilExtensions
         return str.Substring(0, index);
     }
 
-    // indexof string, StringComparison
+    /// <summary>
+    /// Returns a substring from the start of the input string until the first occurrence of the specified delimiter using the specified comparison type.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <param name="comparisonType">The comparison type to use.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(this string str, string until, StringComparison comparisonType)
     {
         if (string.IsNullOrEmpty(until))
@@ -78,7 +129,15 @@ public static class UntilExtensions
         return str.Substring(0, index);
     }
 
-    // indexof string, startindex, count, StringComparison
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified delimiter using the specified comparison type within the specified count.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <param name="count">The maximum number of characters to consider.</param>
+    /// <param name="comparisonType">The comparison type to use.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(this string str, string until, int startIndex, int count, StringComparison comparisonType)
     {
         if (string.IsNullOrEmpty(until))
@@ -88,10 +147,17 @@ public static class UntilExtensions
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
 
-    // indexof string, startindex, count
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified delimiter using the specified count.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <param name="count">The maximum number of characters to consider.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(this string str, string until, int startIndex, int count)
     {
         if (string.IsNullOrEmpty(until))
@@ -101,10 +167,17 @@ public static class UntilExtensions
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
 
-    // indexof string, startindex, StringComparison
+    /// <summary>
+    /// Returns a substring from the specified start index of the input string until the first occurrence of the specified delimiter using the specified comparison type.
+    /// </summary>
+    /// <param name="str">The input string.</param>
+    /// <param name="until">The delimiter string.</param>
+    /// <param name="startIndex">The start index of the substring.</param>
+    /// <param name="comparisonType">The comparison type to use.</param>
+    /// <returns>The substring until the first occurrence of the delimiter, or the original string if the delimiter is not found.</returns>
     public static string Until(this string str, string until, int startIndex, StringComparison comparisonType)
     {
         if (string.IsNullOrEmpty(until))
@@ -114,7 +187,6 @@ public static class UntilExtensions
         if (index == -1)
             return str;
 
-        return str.Substring(startIndex, index-startIndex);
+        return str.Substring(startIndex, index - startIndex);
     }
-
 }
